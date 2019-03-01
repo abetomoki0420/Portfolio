99a8dc86a15b8886a7302828572b6711ce8bdf0e
chachamame <kimi.jack.of.all.trades@gmail.com>
2019-03-01 14:21:21 +0900

    fix router mode

--- src/router.js
+++ src/router.js
@@ -7,8 +7,8 @@ import About from '@/components/About'
 vue.use(VueRouter);
 
 export default new VueRouter({
-  routes: [
-    {
+  mode: 'history',
+  routes: [{
       path: '/',
       component: About
     },
